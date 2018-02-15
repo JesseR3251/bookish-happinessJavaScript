c4d2a5c3d4132d9ef79a441ee5297cd48f2092f7
Student <student@cse.local>
2018-02-15 18:31:13 -0500

    Added a math function to my lab2 javascrip

--- lab2/index.js
+++ lab2/index.js
@@ -6,9 +6,12 @@ function countryToken() {
 	var twoToken = 2;
 	var oneToken = 1;
 	var totalValue = 0;
+	
 	var userInput = prompt("Enter a number: ");
 	while (totalValue < userInput) {
-		totalValue += twentyToken;
+		totalValue = Math.floor((userInput / twentyToken));
+//		break;
+		//twentyToken += totalValue;
 			
 	}
 	document.write(totalValue);

0f7c659ebf6d9623815f1169f69f883a62a5406b
Jesse Robinson <jesse.michael.robinson@gmail.com>
2018-02-14 21:01:08 -0500

    Worked on an algorithm and the actual JavaScript for my lab 2 homework

---
+++ lab2/index.js
@@ -0,0 +1,15 @@
+
+function countryToken() {
+	var twentyToken = 20;
+	var tenToken = 10;
+	var fiveToken = 5;
+	var twoToken = 2;
+	var oneToken = 1;
+	var totalValue = 0;
+	var userInput = prompt("Enter a number: ");
+	while (totalValue < userInput) {
+		totalValue += twentyToken;
+			
+	}
+	document.write(totalValue);
+}

cce3f0c79b990523bf664a0cb71ef9aa322c7244
Jesse Robinson <jesse.michael.robinson@gmail.com>
2018-02-20 16:16:06 -0500

    added an if condition to print the multiplier only if it is being used

--- lab2/index.js
+++ lab2/index.js
@@ -7,33 +7,40 @@ var fiveToken = 5;
 var twoToken = 2;
 var oneToken = 1;
 
-var userInput = prompt("Enter a number: ");
+var userInput = prompt("Enter a number 20 through 100: ");
 
 if (userInput > 19 && userInput < 101) {
 	
 	twentyMultiplier = Math.floor((userInput / twentyToken));
 	var totalValue = twentyToken * twentyMultiplier;
-	document.write('The input entered is ' + userInput + 
+	document.write('The <strong> input </strong> entered is ' + userInput + ' and your <em> change </em> is: ' +
 	'<br>' + twentyToken + ' x ' + twentyMultiplier + '<br>');
 	var remainder = userInput - totalValue;
 
+	
 	var tenMultiplier = Math.floor(remainder / tenToken);
+	if (tenMultiplier != 0) {
 	document.write(tenToken + ' x ' + tenMultiplier + '<br>');
+	}
 	var totalValue = tenToken * tenMultiplier;
 	var remainder = remainder - totalValue;
 
 	var fiveMultiplier = Math.floor(remainder / fiveToken);
+	if (fiveMultiplier != 0) {
 	document.write(fiveToken + ' x ' + fiveMultiplier + '<br>');
+	}
 	var totalValue = fiveToken * fiveMultiplier;
 	var remainder = remainder - totalValue;
 
 	var twoMultiplier = Math.floor(remainder / twoToken);
-	document.write(twoToken + ' x ' + twoMultiplier + '<br>');
+	if (twoMultiplier != 0) {
+	document.write(twoToken + ' x ' + twoMultiplier + '<br>'); }
 	var totalValue = twoToken * twoMultiplier;
 	var remainder = remainder - totalValue;
 
 	var oneMultiplier = Math.floor(remainder / oneToken);
-	document.write(oneToken + ' x ' + oneMultiplier + '<br>');
+	if (oneMultiplier != 0) {
+	document.write(oneToken + ' x ' + oneMultiplier + '<br>'); }
 	var totalValue = oneToken * oneMultiplier;
 	var remainder = remainder - totalValue;
 }

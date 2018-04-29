d26429938a4fad12981644f3a71f29fc47864007
Jesse Robinson <Jesse.michael.robinson@gmail.com>
2018-04-28 20:16:47 -0400

    Completed contact me page

--- final/javascript/contactme-script.js
+++ final/javascript/contactme-script.js
@@ -12,23 +12,72 @@ function whiteEmail() {
 }
 
 function validateField() {
-    var $result = $("#result");
-    var email = $("#email").val();
-    $result.text("");
+    var email = document.getElementById('email').value;
 
     if (emailSubmit(email)) {
         alert('Submitted!')
         document.forms["formInfo"].reset();
+        return true
     } else {
         alert('Please enter a valid email address.');
         highlightEmail();
+        return false;
 
     }
-    return false;
-}
 
-$("#validate").bind("click", validateField);
+}
 
 $(function () {
     $("#datepicker").datepicker();
 });
+
+
+var pass = document.getElementById("password")
+pass.addEventListener('keyup', function () {
+    checkPassword(pass.value)
+})
+
+function checkPassword(password) {
+    var strengthBar = document.getElementById("strength")
+    var strength = 0;
+    if (password.match(/[a-zA-Z0-9][a-zA-Z0-9]+/)) {
+        strength += 1
+    }
+    if (password.match(/[~<>?]+/)) {
+        strength += 1
+    }
+    if (password.match(/[!@£$%^&*()]+/)) {
+        strength += 1
+    }
+    if (password.length > 5) {
+        strength += 1
+    }
+    switch (strength) {
+        case 0:
+            strengthBar.value = 0;
+            break
+        case 1:
+            strengthBar.value = 20;
+            break
+        case 2:
+            strengthBar.value = 50;
+            break
+        case 3:
+            strengthBar.value = 80;
+            break
+        case 4:
+            strengthBar.value = 100;
+            break
+
+    }
+}
+
+var maxAmount = 250;
+
+function textCounter(textField, showCountField) {
+    if (textField.value.length > maxAmount) {
+        textField.value = textField.value.substring(0, maxAmount);
+    } else {
+        showCountField.value = maxAmount - textField.value.length;
+    }
+}
